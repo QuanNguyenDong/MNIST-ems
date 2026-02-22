2a5a7f94d1f2d7f2d4e3de14e7b9f4c3949aafbf
Quan Quan <79500388+QuanNguyenDong@users.noreply.github.com>
2026-02-22 16:55:02 +1100

    Configure backend-go for Heroku

--- backend-go/main.go
+++ backend-go/main.go
@@ -320,6 +320,11 @@ func main() {
 	c := cors.Default()
 	handler := c.Handler(mux)
 
-	log.Println("Listening on :5001...")
-	log.Fatal(http.ListenAndServe(":5001", handler))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "5001"
+	}
+
+	log.Printf("Listening on :%s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
 }
